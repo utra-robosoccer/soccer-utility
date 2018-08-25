f8e965cae6cb2cbe90f66d51b69291f7d417e7e7
Jason Wang <vuwij@me.com>
2018-08-25 15:27:21 -0400

    Update

--- connectRobot.m
+++ connectRobot.m
@@ -1,18 +1,22 @@
 % IP Addresses for robots
 rosshutdown;
 
-localIp = '100.64.41.3';
-gazeboIp = '100.64.38.143';
-robotIp = '100.64.41.3';
+localIp = '100.64.39.142';
+gazeboIp = '100.64.46.29';
+robotIp = '100.64.46.245';
 
-useRobot = 0; 
+useRobot = 1; 
 
 if useRobot
     setenv('ROS_IP', localIp)
     setenv('ROS_MASTER_URI',strcat('http://', robotIp, ':11311'))
     rosinit(robotIp, 'NodeHost', localIp)
+%     device = rosdevice(robotIp, 'nvidia', 'nvidia');
+%     device.ROSFolder = '/opt/ros/kinetic';
+%     device.CatkinWorkspace = '~/soccer_ws';
 else
     setenv('ROS_IP', localIp)
     setenv('ROS_MASTER_URI',strcat('http://', gazeboIp, ':11311'))
     rosinit(gazeboIp, 'NodeHost', localIp)
+    device = rosdevice(gazeboIp,'vuwij','sh961013');
 end
